7f72ee10160346844dcbaa5caee5b9b782bda8d3
rasmus.stene <rasmus.stene@cgi.com>
2020-11-13 10:05:16 +0100

    Add flutter-tagging widget and simple recipe-list

--- lib/main.dart
+++ lib/main.dart
@@ -32,18 +32,9 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  List<Ingredient> _selectedIngredients = [];
+  List<Ingredient> _previouslySelectedIngredients = [];
+  Map<String, dynamic> _recipes = {};
 
   @override
   Widget build(BuildContext context) {
@@ -55,22 +46,87 @@ class _MyHomePageState extends State<MyHomePage> {
         Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(
-              'You have pressed the button this many times:',
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: FlutterTagging(
+                initialItems: _selectedIngredients,
+                findSuggestions: fetchIngredientsContaining,
+                configureChip: (ingredient) {
+                  return ChipConfiguration(
+                    label: Text(ingredient.name),
+                    backgroundColor: Colors.blue,
+                    labelStyle: TextStyle(color: Colors.white),
+                    deleteIconColor: Colors.white
+                  );
+                },
+                configureSuggestion: (ingredient) {
+                  return SuggestionConfiguration(title: Text(ingredient.name));
+                },
+                onChanged: () {
+                  selectedIngredientsChanged();
+                },
+                textFieldConfiguration: TextFieldConfiguration(
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                    filled: true,
+                    fillColor: Colors.blue.withAlpha(30),
+                    hintText: 'Søk etter ingredienser...',
+                    labelText: 'Ingredienssøk'
+                  )
+                ),
+              )
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
+            ListView.separated(
+              physics: ClampingScrollPhysics(),
+              shrinkWrap: true,
+              padding: const EdgeInsets.all(8),
+              itemCount: _recipes.length,
+              itemBuilder: (BuildContext context, int index) {
+                String ingredientName = _recipes.keys.elementAt(index);
+                return Container(
+                    child: Column(
+                      children: <Widget>[
+                        Text(
+                            ingredientName,
+                            style: TextStyle(fontWeight: FontWeight.bold)
+                        ),
+                        ListView.builder(
+                            physics: ClampingScrollPhysics(),
+                            shrinkWrap: true,
+                            itemCount: _recipes[ingredientName].length,
+                            itemBuilder: (BuildContext context, int index) {
+                              return Text(_recipes[ingredientName][index].recipeName);
+                            }
+                        ),
+                      ],
+                    )
+                );
+              },
+              separatorBuilder: (BuildContext context, int index) => const Divider(),
+            )
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+
+  void selectedIngredientsChanged() {
+    if (_selectedIngredients.length > _previouslySelectedIngredients.length) {
+      List<Ingredient> selectionDifference = _selectedIngredients.toSet().difference(_previouslySelectedIngredients.toSet()).toList();
+      selectionDifference.forEach((ingredient) async {
+        final recipeIngredients = await ingredient.fetchRecipes();
+        setState(() {
+          _recipes[ingredient.name] = recipeIngredients;
+        });
+      });
+    } else {
+      List<Ingredient> selectionDifference = _previouslySelectedIngredients.toSet().difference(_selectedIngredients.toSet()).toList();
+      selectionDifference.forEach((ingredient) {
+        setState(() {
+          _recipes.remove(ingredient.name);
+        });
+      });
+    }
+    _previouslySelectedIngredients = new List<Ingredient>.from(_selectedIngredients);
   }
 }
 
